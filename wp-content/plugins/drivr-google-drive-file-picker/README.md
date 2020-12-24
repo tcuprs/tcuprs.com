6512e662f4779643161886f27f590c5c2c8e11e3
tcuprs <info@tcuprs.com>
2020-12-25 05:56:55 +0900

    gitignore fixed-2

--- wp-content/plugins/drivr-google-drive-file-picker/README.md
+++
@@ -1,2 +0,0 @@
-# Drivr-Lite-Google-Drive-Plugin
-A wordpress plugin that helps you to access files from your Google Drive Account quickly and seamlessly.
--- wp-content/plugins/xserver-migrator/packages/class-xserver-migrator-exceptions.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-class Xserver_Migrator_Archive_Exception extends Exception {}
-class Xserver_Migrator_Database_Dump_Exception extends Exception {}
